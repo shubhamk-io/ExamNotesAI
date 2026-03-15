56392df29d6f023ce00cd4b12e7e3257870ef2e4
shubham kumar <shubhamk16990@gmail.com>
2026-03-15 14:55:54 +0530

    creating slibar and final section

--- frontend/src/components/FinalResul.jsx
+++ frontend/src/components/FinalResul.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 
-const FinalResul = ({result}) => {
+
+const markDownComponent = () => {
+  h1: ({ childern }) => {
+    return <h1 className='text-2xl font-bold text-indigo-700 mt-6 mb-4 border-b pb-2 '>
+      {childern}
+    </h1>
+  }
+  h2: ({ childern }) => {
+    return <h2 className='text-xl font-semibold text-indigo-600 mt-5 mb-3 '>
+      {childern}
+    </h2>
+  }
+  h3: ({ childern }) => {
+    return <h3 className='text-lg font-semibold text-gray-800 mt-4 mb-2 '>
+      {childern}
+    </h3>
+  }
+  p: ({ childern }) => {
+    return <p className='text-gray-700 leading-relaxed mb-3  '>
+      {childern}
+    </p>
+  }
+  ul: ({ childern }) => {
+    return <ul className='list-disc ml-6 space-y-1 text-gray-700  '>
+      {childern}
+    </ul>
+  }
+  li: ({ childern }) => {
+    return <li className='marker:text-indigo-500 '>
+      {childern}
+    </li>
+  }
+}
+
+const FinalResul = ({ result }) => {
+
+  if (!result ||
+    !result.subTopics ||
+    !result.questions ||
+    !result.questions.short ||
+    !result.questions.long ||
+    !result.revisionPoints
+  ) {
+    return null;
+  }
+
+
   return (
-    <div>FinalResul</div>
+    <div className='mt-6 p-3 space-y-10 bg-white'>
+      <div className='flex flex-col md:flex-row md:items-center justify-between gap-4'>
+
+        <h2 className='text-3xl font-bold bg-gradient-to-br from-indigo-600 to-purple-600 bg-clip-text text-transparent'>
+          📘 Generated Notes
+        </h2>
+        <div className='flex gap-3'>
+          <button className=''></button>
+          <button></button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,27 +1,97 @@
 import React from "react";
 
 const SideBar = ({ result }) => {
-    if (
-        !result ||
-        !result.questions ||
-        !result.questions.short ||
-        !result.questions.long
-    ) {
-        return null;
-    }
+  if (
+    !result ||
+    !result.subTopics ||
+    !result.questions ||
+    !result.questions.short ||
+    !result.questions.long
+  ) {
+    return null;
+  }
 
-    return (
-        <div className="bg-white rounded-2xl border border-gray-200 shadow-sm space-y-6">
-            <div className="flex items-center gap-2">
+  return (
+    <div className="bg-white rounded-2xl border border-gray-200 shadow-sm space-y-6">
+      <div className="flex items-center gap-2">
+        <span className="text-xl">📌</span>
+        <h3 className="font-semibold text-indigo-600 text-lg">
+          Quick Exam View
+        </h3>
+      </div>
 
-<span className="text-xl">📌</span>
-<h3 className="font-semibold text-indigo-600 text-lg">
-    Quick Exam View
-</h3>
+      <section>
+        <p className="text-sm font-semibold text-gray-700 mb-3">
+          Topic (Prioity wish)
+        </p>
 
+        {Object.entries(result.subTopics).map(([star, topics]) => {
+          return (
+            <div
+              key={star}
+              className="mb-3 rounded-lg bg-gray-50 border border-gray-200 p-3"
+            >
+              <p className="text-sm font-semibold text-yellow-600 mb-1 ">
+                {star} Prioity
+              </p>
+              <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1">
+                {topics.map((t, i) => {
+                  return <li key={i}>{t}</li>;
+                })}
+              </ul>
             </div>
+          );
+        })}
+      </section>
+
+      <section className="rounded-lg bg-yellow-50  border border-yellow-200 p-3">
+        <p className="text-sm font-semibold text-gray-700 mb-1">
+          🔥 Exam Importance
+        </p>
+        <span className="text-yellow-700 font-bold text-sm ">
+          {result.importance}
+        </span>
+
+        <p className="text-sm font-semibold mt-2 text-gray-700 mb-3"> ❓Important Question </p>
+        <div className="mb-4 rounded-lg bg-indigo-50 border border-indigo-200 p-3">
+
+          <p className="text-sm font-medium text-indigo-700 mb-2">
+            Short Questions
+          </p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1 ">
+            {result.questions.short.map((t, i) => {
+              return <li key={i}>{t}</li>
+            })}
+          </ul>
         </div>
-    );
+
+        <div className="mb-4 rounded-lg bg-purple-50 border border-purple-200 p-3">
+
+          <p className="text-sm font-medium text-purple-700 mb-2">
+            Long Questions
+          </p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1 ">
+            {result.questions.long.map((t, i) => {
+              return <li key={i}>{t}</li>
+            })}
+          </ul>
+        </div>
+
+        <div className="mb-4 rounded-lg bg-blue-50 border border-blue-200 p-3">
+
+          <p className="text-sm font-medium text-blue-700 mb-2">
+            Diagram Questions
+          </p>
+          <ul className="list-disc ml-4 text-sm text-gray-700 space-y-1 ">
+            <li> {result.questions.diagram}</li>
+          </ul>
+        </div>
+
+      </section>
+
+
+    </div>
+  );
 };
 
 export default SideBar;
